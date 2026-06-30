82b1a4d3e4ecc38c9aed957836182d9e3b81019e
Pedro <joaopedropereira2209@gmail.com>
2026-06-30 14:23:07 -0300

    feat: configure express server

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    response.status(200).json({
+        sucess: true,
+        message: "API do Sistema de Gestão para Empresas de Mineraação"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado na porta ${PORT}`);
+});
