66b2dc7f451267327c0947fb61d90d2dc161cfad
Pedro <joaopedropereira2209@gmail.com>
2026-06-30 14:29:58 -0300

    feat: configure cors middleware

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
+const cors = require("./config/cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors);
 app.use(express.json());
 
 app.get("/", (request, response) => {
