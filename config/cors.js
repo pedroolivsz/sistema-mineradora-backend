0038cc8bb0d0766bb350811c08ffe50a69fc0f1d
Pedro <joaopedropereira2209@gmail.com>
2026-07-01 15:54:40 -0300

    fix: update cors origins03

--- config/cors.js
+++ config/cors.js
@@ -1,18 +1,25 @@
+const cors = require("cors");
+
 const allowedOrigins = [
     "localhost",
     "gestao-de-mineradoras.netlify.app"
 ];
 
-origin(origin, callback) {
-    if (!origin) return callback(null, true);
+module.exports = cors({
+    origin(origin, callback) {
+        if (!origin) return callback(null, true);
 
-    const isAllowed = allowedOrigins.some(domain =>
-        origin.includes(domain)
-    );
+        const isAllowed = allowedOrigins.some(domain =>
+            origin.includes(domain)
+        );
 
-    if (isAllowed) {
-        return callback(null, true);
-    }
+        if (isAllowed) {
+            return callback(null, true);
+        }
 
-    return callback(new Error("Not allowed by CORS"));
-}
+        return callback(new Error("Not allowed by CORS"));
+    },
+
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+});
