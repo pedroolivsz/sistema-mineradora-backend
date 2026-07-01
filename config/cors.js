66434644854e006d33b89483113bfdd655f8f910
Pedro <joaopedropereira2209@gmail.com>
2026-07-01 15:03:09 -0300

    ajuste de cors

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,10 @@
 const cors = require("cors");
 
 const corsOptions = {
-origin: "http://localhost:5173",
+origin: [
+        "http://localhost:5173",
+        "https://gestao-de-mineradoras.netlify.app/"
+    ],
 methods: ["GET", "POST", "PUT", "DELETE"],
 credentials: true
 };
